a4c912a7269a8a55beb30c3f9440146ebee48ac1
Hemedi Saidi <shemedi99@gmail.com>
2016-10-01 18:31:48 +0930

    Creating custom HTML Elements

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ node.classList.add('class')
 // Clone a dom node
 let node = document.getElementById('id')
 node.cloneNode(true) // true for a deep node
+
+
+
+// Creating custom HTML Elements
+// more info (https://www.w3.org/TR/custom-elements/)
+class RadioButton extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.setAttribute('role', 'radio');
+    this.setAttribute('tabindex', -1);
+    this.setAttribute('aria-checked', false);
+  }
+}
+
+window.customElements.define('radio-button', RadioButton);
