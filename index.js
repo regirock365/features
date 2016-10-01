090f097b338481660e12640b861db1697d6f4df2
Hemedi Saidi <shemedi99@gmail.com>
2016-10-01 18:23:52 +0930

    Creating DOM elements in JS

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 /*
 * JS FEATURES
 */
+
+// Creating elements in JS
+let div = document.createElement('div')
+let domDestination = document.getElementById('destination')
+domDestination.appendChild(div)
+
+// Adding a class to an element
+let node = document.getElementById('id')
+node.classList.add('class')
+
+// Clone a dom node
+let node = document.getElementById('id')
+node.cloneNode(true) // true for a deep node
